e6e151fc68611a769dc5c046ef4e53b3107cda7e
Arthur Sant'Anna <arthur.silveira@sempreceub.com>
2025-11-19 10:17:43 -0300

    Add files via upload

--- ControleProjeto.sql
+++ ControleProjeto.sql
@@ -2,13 +2,13 @@ CREATE DATABASE controleprojetos;
 USE controleprojetos;
 
 CREATE TABLE Responsavel (
-    id_responsavel INT AUTO_INCREMENT PRIMARY KEY,
+    id_responsavel LONG AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE Projeto (
-    id_projeto INT AUTO_INCREMENT PRIMARY KEY,
+    id_projeto LONG AUTO_INCREMENT PRIMARY KEY,
     nome_projeto VARCHAR(150) NOT NULL,
     data_inicio DATE,
     
@@ -17,12 +17,14 @@ CREATE TABLE Projeto (
 );
 
 CREATE TABLE Tarefa (
-    id_tarefa INT AUTO_INCREMENT PRIMARY KEY,
+    id_tarefa LONG AUTO_INCREMENT PRIMARY KEY,
     descricao VARCHAR(255) NOT NULL,
-    status_tarefa VARCHAR(50) DEFAULT 'Pendente',
+    status_tarefa VARCHAR(50),
     
     id_projeto INT NOT NULL,
     FOREIGN KEY (id_projeto) 
         REFERENCES Projeto(id_projeto)
         ON DELETE CASCADE 
 );
+
+SELECT * FROM TAREFA;
